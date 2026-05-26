117df1863d621810bb87a68a048f525bbaf5ba0e
Nahuel Cáceres <nahuel@localhost.localdomain>
2026-05-26 13:06:12 -0300

    Parcial sin dos puntos

---
+++ .stack-work/dist/x86_64-linux/ghc-9.6.6/build/autogen/Paths_repoBase.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+#if __GLASGOW_HASKELL__ >= 810
+{-# OPTIONS_GHC -Wno-prepositive-qualified-module #-}
+#endif
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module Paths_repoBase (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+
+import qualified Control.Exception as Exception
+import qualified Data.List as List
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir `joinFileName` name)
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+
+
+
+
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+bindir     = "/home/nahuel/UTN/Paradigmas/parciales/practicas/Funcional---Jack/.stack-work/install/x86_64-linux/38bef4cd4888f8a524c9295e7d539ca68877cf841b098a78a9d1d1a5220a1e32/9.6.6/bin"
+libdir     = "/home/nahuel/UTN/Paradigmas/parciales/practicas/Funcional---Jack/.stack-work/install/x86_64-linux/38bef4cd4888f8a524c9295e7d539ca68877cf841b098a78a9d1d1a5220a1e32/9.6.6/lib/x86_64-linux-ghc-9.6.6/repoBase-0.1.0.0-4BT3AmIYla3Fb257nEIo2D"
+dynlibdir  = "/home/nahuel/UTN/Paradigmas/parciales/practicas/Funcional---Jack/.stack-work/install/x86_64-linux/38bef4cd4888f8a524c9295e7d539ca68877cf841b098a78a9d1d1a5220a1e32/9.6.6/lib/x86_64-linux-ghc-9.6.6"
+datadir    = "/home/nahuel/UTN/Paradigmas/parciales/practicas/Funcional---Jack/.stack-work/install/x86_64-linux/38bef4cd4888f8a524c9295e7d539ca68877cf841b098a78a9d1d1a5220a1e32/9.6.6/share/x86_64-linux-ghc-9.6.6/repoBase-0.1.0.0"
+libexecdir = "/home/nahuel/UTN/Paradigmas/parciales/practicas/Funcional---Jack/.stack-work/install/x86_64-linux/38bef4cd4888f8a524c9295e7d539ca68877cf841b098a78a9d1d1a5220a1e32/9.6.6/libexec/x86_64-linux-ghc-9.6.6/repoBase-0.1.0.0"
+sysconfdir = "/home/nahuel/UTN/Paradigmas/parciales/practicas/Funcional---Jack/.stack-work/install/x86_64-linux/38bef4cd4888f8a524c9295e7d539ca68877cf841b098a78a9d1d1a5220a1e32/9.6.6/etc"
+
+getBinDir     = catchIO (getEnv "repoBase_bindir")     (\_ -> return bindir)
+getLibDir     = catchIO (getEnv "repoBase_libdir")     (\_ -> return libdir)
+getDynLibDir  = catchIO (getEnv "repoBase_dynlibdir")  (\_ -> return dynlibdir)
+getDataDir    = catchIO (getEnv "repoBase_datadir")    (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "repoBase_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "repoBase_sysconfdir") (\_ -> return sysconfdir)
+
+
+
+joinFileName :: String -> String -> FilePath
+joinFileName ""  fname = fname
+joinFileName "." fname = fname
+joinFileName dir ""    = dir
+joinFileName dir fname
+  | isPathSeparator (List.last dir) = dir ++ fname
+  | otherwise                       = dir ++ pathSeparator : fname
+
+pathSeparator :: Char
+pathSeparator = '/'
+
+isPathSeparator :: Char -> Bool
+isPathSeparator c = c == '/'
---
+++ src/Library.hs
@@ -0,0 +1,123 @@
+module Library where
+import PdePreludat
+
+doble :: Number -> Number
+doble numero = numero + numero
+
+type Velocidad = Number
+type Distancia = Number
+type Color = String
+data Auto = Auto{
+  color :: Color,
+  velocidad :: Velocidad,
+  distancia :: Number
+}
+
+type Carrera = [Auto]
+
+-- 1
+estaCerca :: Auto -> Auto -> Bool
+estaCerca auto1 auto2 = (sonDistintos auto1 auto2 ) && ((<10) . distanciaEntre auto1 $ auto2)
+
+sonDistintos :: Auto -> Auto -> Bool
+sonDistintos auto1 auto2 = (/= color auto1) . color $ auto2
+
+distanciaEntre :: Auto -> Auto -> Number
+distanciaEntre auto1 auto2 = abs . difDistancia auto1 $ auto2
+
+difDistancia :: Auto -> Auto -> Number
+difDistancia auto1 auto2 = (subtract . distancia $ auto1) . distancia $ auto2
+
+vaTranquilo :: Auto -> Carrera -> Bool
+vaTranquilo auto carrera = any (estaCerca auto) carrera && ((==1) . puesto auto $ carrera)
+
+puesto :: Auto -> Carrera -> Number
+puesto auto carrera = (+1) . length . filter (estaDelante auto) $ carrera
+
+estaDelante :: Auto -> Auto -> Bool
+estaDelante auto1 auto2 = (< 0) . difDistancia auto1 $ auto2
+
+-- 2
+-- a
+type Tiempo = Number
+correr :: Tiempo -> Auto -> Auto
+correr tiempo auto = avanzar (recorrer tiempo auto) auto  
+
+avanzar :: Distancia -> Auto -> Auto
+avanzar nuevaDistancia auto = auto {distancia = (+ nuevaDistancia) . distancia $ auto}
+
+recorrer :: Tiempo -> Auto -> Number
+recorrer tiempo auto = (* tiempo).velocidad $ auto
+
+-- b
+alterarVelocidad :: (Number -> Number) -> Auto -> Auto
+alterarVelocidad modificador auto = auto { velocidad = max 0 . modificador.velocidad $ auto }
+
+bajarVelocidad :: Number -> Auto -> Auto
+bajarVelocidad numero auto = flip alterarVelocidad auto . subtract $ numero
+
+-- 3
+type PowerUp = Auto -> Carrera -> Carrera
+
+afectarALosQueCumplen :: (a -> Bool) -> (a -> a) -> [a] -> [a]
+afectarALosQueCumplen criterio efecto lista
+  = (map efecto . filter criterio) lista ++ filter (not.criterio) lista
+
+
+terremoto :: PowerUp
+terremoto auto carrera = afectarALosQueCumplen (estaCerca auto) (alterarVelocidad (subtract 50)) carrera
+
+miguelito :: Velocidad -> PowerUp
+miguelito velocidad auto carrera = afectarALosQueCumplen (estaDelante auto) (bajarVelocidad velocidad) carrera
+
+jetPack :: Tiempo -> PowerUp
+--jetPack tiempo auto carrera = afectarALosQueCumplen (not sonDistintos auto) (usarJetpack) carrera
+jetPack = undefined
+
+-- 4
+-- a
+type Eventos = [Carrera->Carrera]
+type Posiciones = (Number, Color)
+
+simularCarrera :: Carrera -> Eventos -> [Posiciones]
+simularCarrera carrera eventos = hacerPodio . foldr ($) carrera $ eventos
+
+hacerPodio :: Carrera -> [Posiciones]
+hacerPodio carreraTerminada = map (posicion carreraTerminada) carreraTerminada
+
+posicion :: Carrera -> Auto -> Posiciones
+posicion carreraTerminada auto = (puesto auto carreraTerminada, color auto)
+
+-- b
+correnTodos :: Tiempo -> Carrera -> Carrera
+correnTodos tiempo carrera = map (correr tiempo) carrera
+
+usaPowerUp :: PowerUp -> Color -> Carrera -> Carrera
+--usaPowerUp powerUp color carrera = powerUp . filter  
+usaPowerUp = undefined
+
+-- c
+autoIncial = Auto{
+  color = "",
+  velocidad = 120,
+  distancia = 0
+}
+
+rojo = autoIncial{color = "Rojo"}
+blanco = autoIncial{color = "blanco"}
+azul = autoIncial{color = "Azul"}
+negro = autoIncial{color = "Negro"}
+
+eventos = [correnTodos 30, usaPowerUp (jetPack 3) "Azul", terremoto blanco, correnTodos 40, usaPowerUp (miguelito 20) "Blanco", usaPowerUp (jetPack 6) "Negro", correnTodos 10]
+unaCarrera = [rojo, blanco, azul, negro]
+
+ejemplo = simularCarrera unaCarrera eventos
+
+-- 5a
+-- Es posible ya que la definición de powerUp permite agregarlo. Es solamente declararlo como
+-- misilTeledirigido :: Auto -> PowerUp
+
+-- 5b
+-- 1b -> VaTranquilo podría llegar a terminar por el lazy evaluation, al encontrar un auto
+-- que rompa la búsqueda
+-- 1c -> puesto nunca va a terminar porque evaluaría todos los autos de la carrera, y al ser una lista infinita eso nunca pasaría
